b77c8c59789f5878487c2faa4867834fcd7e2238
Arne Bahlo <hallo@arne.me>
2014-06-12 00:30:00 +0200

    Add test template

--- swift-gifTests/GifTests.swift
+++ swift-gifTests/GifTests.swift
@@ -1,35 +1,35 @@
 //
-//  swift_gifTests.swift
-//  swift-gifTests
+//  GifTests.swift
+//  swift-gif
 //
-//  Created by Arne Bahlo on 10.06.14.
+//  Created by Arne Bahlo on 12.06.14.
 //  Copyright (c) 2014 Arne Bahlo. All rights reserved.
 //
 
 import XCTest
 
-class swift_gifTests: XCTestCase {
-    
+class GifTests: XCTestCase {
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
